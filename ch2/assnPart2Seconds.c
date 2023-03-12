2dff20f2d27eb8fbf8dc3644f7226085dc79fb98
Anurag <shuklaanurag998@gmail.com>
2023-03-12 13:27:56 +0530

    Added ch 2

---
+++ ch2/assnPart2Seconds.c
@@ -0,0 +1,108 @@
+/**
+ * hello.c
+ *
+ * Kernel module that communicates with /proc file system.
+ * 
+ * The makefile must be modified to compile this program.
+ * Change the line "simple.o" to "hello.o"
+ *
+ * Operating System Concepts - 10th Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+#include <linux/jiffies.h>
+#include <asm/param.h>
+#define BUFFER_SIZE 128
+
+#define PROC_NAME "seconds"
+#define MESSAGE "Hello World\n"
+
+/**
+ * Function prototypes
+ */
+ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
+
+static struct file_operations proc_ops = {
+        .owner = THIS_MODULE,
+        .read = proc_read,
+};
+
+/* Defining a global variable for storing the value of jiffies when the module is loaded */
+
+unsigned long _init_jiffies;
+
+/* This function is called when the module is loaded. */
+int proc_init(void)
+{
+
+        // creates the /proc/hello entry
+        // the following function call is a wrapper for
+        // proc_create_data() passing NULL as the last argument
+        proc_create(PROC_NAME, 0, NULL, &proc_ops);
+
+        printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+	
+	_init_jiffies = jiffies;	
+
+	return 0;
+}
+
+/* This function is called when the module is removed. */
+void proc_exit(void) {
+
+        // removes the /proc/hello entry
+        remove_proc_entry(PROC_NAME, NULL);
+
+        printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
+}
+
+/**
+ * This function is called each time the /proc/hello is read.
+ * 
+ * This function is called repeatedly until it returns 0, so
+ * there must be logic that ensures it ultimately returns 0
+ * once it has collected the data that is to go into the 
+ * corresponding /proc file.
+ *
+ * params:
+ *
+ * file:
+ * buf: buffer in user space
+ * count:
+ * pos:
+ */
+ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+        int rv = 0;
+        char buffer[BUFFER_SIZE];
+        static int completed = 0;
+
+        if (completed) {
+                completed = 0;
+                return 0;
+        }
+
+        completed = 1;
+
+        rv = sprintf(buffer, "Time(in seconds) since the module is loaded is : %lu \n", (jiffies - _init_jiffies)/HZ);
+
+        // copies the contents of buffer to userspace usr_buf
+        copy_to_user(usr_buf, buffer, rv);
+
+        return rv;
+}
+
+
+/* Macros for registering module entry and exit points. */
+module_init( proc_init );
+module_exit( proc_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Hello Module");
+MODULE_AUTHOR("SGG");
+
